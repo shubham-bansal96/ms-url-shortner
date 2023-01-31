62c139e76d75265013091f228f7fc9ea259367c5
Shubham Bansal <shubhambansaliimtgn@gmail.com>
2023-01-31 23:03:08 +0530

    initial commit for prometheus

--- app/route/route.go
+++ app/route/route.go
@@ -5,12 +5,22 @@ import (
 	"github.com/ms-url-shortner/app/config"
 	"github.com/ms-url-shortner/app/controller"
 	"github.com/ms-url-shortner/app/services"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func Initialize(router *gin.Engine) {
+	router.GET("/metrics", gin.WrapH(promhttp.Handler()))
 	ctrl := controller.NewBaseContoller(services.NewShortenURLService(services.NewUidService()))
 
 	appGroup := router.Group("/" + config.Config.MSName)
 	appGroup.GET(testEndPoint, ctrl.Ping)
 	appGroup.POST(getShortURLEndPoint, ctrl.HandleURLShortner)
+}
+
+func prometheusHandler() gin.HandlerFunc {
+	h := promhttp.Handler()
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
 }
